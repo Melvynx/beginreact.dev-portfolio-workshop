d7130632414b9912b6369acb002c29591d6c4621
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2022-09-23 15:57:13 +0200

    fix hero

--- src/components/hero/HeroSection.jsx
+++ src/components/hero/HeroSection.jsx
@@ -1,23 +1,8 @@
-import { FULL_NAME } from '../../lib/config';
+g import { FULL_NAME } from '../../lib/config';
 
 export const HeroSection = () => {
   return (
-    <div className="">
-      {/*Dark Mode - Exercise*/}
-      <div className="">
-        {/*Dark Mode - Exercise*/}
-        <h1 className="text-7xl">
-          I'm{' '}
-          <span className="font-extrabold text-transparent bg-gradient-to-r from-primary to-secondary bg-clip-text">
-            {FULL_NAME}
-          </span>
-        </h1>
-        <p className="text-xl">
-          <b>Apprenti React.</b> I'm a software developer that make thing on
-          internet, very happy to see your here, place holder please fill something
-          here please fill something here.
-        </p>
-      </div>
+    <div className="relative">
       <img
         width={300}
         height={300}
@@ -25,7 +10,10 @@ export const HeroSection = () => {
         alt="avatar"
         className="top-[-16px] right-[-42px] rounded shadow-lg md:absolute"
       />
-      <div className="flex flex-col gap-4 md:relative md:mr-16">
+      {/*Dark Mode - Exercise*/}
+      <div className="">
+        {' '}
+        {/*Dark Mode - Exercise*/}
         <h1 className="text-6xl drop-shadow-[0_0px_20px_rgba(0,0,0,0.25)] md:text-8xl">
           I'm{' '}
           <span className="font-extrabold text-transparent bg-gradient-to-r from-primary to-secondary bg-clip-text">
